543f02fd8898e67fa3cb30de438c2442a73ed606
Bazylek12 <a.kosmowski7@gmail.com>
2022-11-27 16:39:05 +0100

    feat: list of public holidays implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,24 @@ import {RouterModule} from '@angular/router';
 import {ProductListComponent} from './components/product-list/product-list.component';
 import {CategoryListComponent} from './components/category-list/category-list.component';
 import {CryptoListComponent} from './components/crypto-list/crypto-list.component';
+import {HolidaysListComponent} from './components/holidays-list/holidays-list.component';
 import {ProductListComponentModule} from './components/product-list/product-list.component-module';
 import {ProductsServiceModule} from './services/products.service-module';
 import {CategoryListComponentModule} from './components/category-list/category-list.component-module';
 import {CategoriesServiceModule} from './services/categories.service-module';
 import {CryptoListComponentModule} from './components/crypto-list/crypto-list.component-module';
 import {CryptosServiceModule} from './services/cryptos.service-module';
+import {HolidaysListComponentModule} from './components/holidays-list/holidays-list.component-module';
+import {HolidaysServiceModule} from './services/holidays.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([{
-    path: 'products',
-    component: ProductListComponent}, {
-    path: 'categories',
-    component: CategoryListComponent
+    path: 'products', component: ProductListComponent}, {
+    path: 'categories', component: CategoryListComponent
   }, {
-    path: 'crypto',
-    component: CryptoListComponent
-  }]), ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoriesServiceModule, CryptoListComponentModule, CryptosServiceModule],
+    path: 'crypto', component: CryptoListComponent}, {
+    path: 'public-holidays', component: HolidaysListComponent
+  }]), ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoriesServiceModule, CryptoListComponentModule, CryptosServiceModule, HolidaysListComponentModule, HolidaysServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
